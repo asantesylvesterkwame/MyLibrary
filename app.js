01c1d0b5d0611aeaf3672ec43f21af3d4d3b9791
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-21 02:02:57 +0000

    created the logic for pop-up form

---
+++ app.js
@@ -0,0 +1,5 @@
+let addBookButton = document.getElementById('AddBookBtn');
+let popUpCard = document.getElementById('popup-card');
+addBookButton.addEventListener('click', function () {
+    popUpCard.style.display = 'block';
+});
