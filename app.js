09e8ce2f555d04a9492028a8e1d5baf32e9d381c
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-21 02:46:02 +0000

    cleaned up my code

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 let addBookButton = document.getElementById('AddBookBtn');
 let popUpCard = document.getElementById('popup-card');
+
 addBookButton.addEventListener('click', function () {
     popUpCard.style.display = 'block';
 });
+
 let popUpCloseBtn = document.getElementById('popUpCloseBtn');
 popUpCloseBtn.addEventListener('click', function () {
     popUpCard.style.display = 'none';
 });
+
+
 
