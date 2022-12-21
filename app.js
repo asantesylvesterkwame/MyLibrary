36a3d1c50547ceb9c099d67f443a57cc7a01f6e6
asantesylvesterkwame <intelligentson17@gmail.com>
2022-12-21 02:41:13 +0000

    created popupclosebtn command logic

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ let popUpCard = document.getElementById('popup-card');
 addBookButton.addEventListener('click', function () {
     popUpCard.style.display = 'block';
 });
+let popUpCloseBtn = document.getElementById('popUpCloseBtn');
+popUpCloseBtn.addEventListener('click', function () {
+    popUpCard.style.display = 'none';
+});
+
