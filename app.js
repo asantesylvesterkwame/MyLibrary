a19b77f3a1d3556d6442add4a7490d0b5f5e5d7e
asantesylvesterkwame <intelligentson17@gmail.com>
2023-01-10 04:48:55 +0000

    created the functionality for appending elements of submit button click

--- app.js
+++ app.js
@@ -7,29 +7,103 @@ function Book(title, author, numberOfPages, readStatus) {
   this.readStatus = readStatus
 }
 
-const bookObject = new Book(prompt('enter the book Title'), prompt('Enter The Book Author'), prompt('Enter The Number of Pages'), prompt('State whether you read it or not'))
-myLibrary.push(bookObject);
 
 console.log(myLibrary);
 
-let bookDisplay = document.getElementById('bookDisplay')
-let bookAuthor = document.getElementById('bookAuthor')
 
 
-function addBookToLibrary() {
-    for (const i of myLibrary) {
-        // bookDisplay.innerHTML = i.title
-        // bookAuthor.innerHTML = i.author
-        // bookDisplay.innerHTML = i.numberOfPages
-        // bookDisplay.innerHTML = i.readStatus
-    }
-}
-addBookToLibrary();
 
 let addBookBtn = document.getElementById('AddBookBtn');
 
 let popUpCard = document.getElementById('popup-card')
 addBookBtn.addEventListener('click', function () {
     popUpCard.style.display = 'block';
-
+    
+    
 });
+
+let submitButton = document.getElementById('submit');
+
+let readStatusCheckBox = document.getElementById('checkbox');
+
+submitButton.addEventListener('click', SubmitBtnFunction)
+function SubmitBtnFunction (e) {
+    e.preventDefault()
+    let bookTitleValue = document.getElementById('bookTitle').value;
+    let bookAuthorValue = document.getElementById('bookAuthor').value;
+    let bookPages = document.getElementById('bookPages').value;
+     
+
+    popUpCard.style.display = 'none'
+    let enable = () => {
+        if (readStatusCheckBox.checked) {
+            return 'Read';
+        } else {
+            return 'Unread';
+        }
+    }
+    enable();
+    let myBookInfo = new Book(bookTitleValue, bookAuthorValue, bookPages,readStatusCheckBox);
+
+    myLibrary.push(myBookInfo);
+    console.log(myLibrary);
+
+    
+    let bookCardDiv = document.getElementById('card-body');
+   
+
+    let bookCardBody = document.createElement('div');
+    bookCardBody.style.cssText = 'background-color: silver;';
+    bookCardBody.setAttribute('id', 'cardBody');
+    bookCardDiv.append(bookCardBody);
+
+    let bookInfoIcon = document.createElement('img');
+    bookInfoIcon.setAttribute('class', 'card-img-top');
+    bookInfoIcon.setAttribute('src', 'book.png');
+    bookInfoIcon.setAttribute('alt', 'Logo');
+    bookInfoIcon.style.cssText = 'width: 25%; height: auto;';
+    bookCardBody.appendChild(bookInfoIcon);
+
+    let cardInfoBody = document.createElement('div');
+    cardInfoBody.setAttribute('class', 'card-body cardInfoBody')
+    bookCardBody.appendChild(cardInfoBody);
+
+    let cardInfoTitle = document.createElement('h4');
+    cardInfoTitle.setAttribute('class', 'card-title');
+    cardInfoTitle.setAttribute('id', 'cardInfoTitle');
+    cardInfoBody.appendChild(cardInfoTitle);
+
+    let cardInfoAuthor = document.createElement('p');
+    cardInfoAuthor.setAttribute('class', 'card-text');
+    cardInfoAuthor.setAttribute('id', 'cardInfoAuthor');
+    cardInfoBody.appendChild(cardInfoAuthor);
+
+    let cardInfoPages = document.createElement('p');
+    cardInfoPages.setAttribute('class', 'card-text');
+    cardInfoPages.setAttribute('id', 'cardInfoPages');
+    cardInfoBody.appendChild(cardInfoPages);
+
+    let unreadButton = document.createElement('input');
+    unreadButton.setAttribute('class', 'btn btn-primary unread');
+    unreadButton.setAttribute('id', 'unreadButton');
+    unreadButton.setAttribute('type', 'button');
+    cardInfoBody.appendChild(unreadButton);
+
+    let removeBook = document.createElement('input');
+    removeBook.setAttribute('class', 'btn btn-primary submit removeBook');
+    removeBook.setAttribute('id', 'unreadButton');
+    removeBook.setAttribute('type', 'button');
+    cardInfoBody.appendChild(removeBook);
+
+    function addBookToLibrary() {
+    for (const i of myLibrary) {
+        cardInfoTitle.innerHTML = bookTitleValue;
+        cardInfoAuthor.innerHTML = bookAuthorValue;
+        cardInfoPages.innerHTML = bookPages;
+        unreadButton.value = enable();
+        removeBook.value = 'Remove Book'
+    }
+}
+addBookToLibrary();
+
+}
